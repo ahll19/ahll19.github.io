6a0dad208f46422672b22d31c235a4faa3f39c22
Anders Lauridsen <ahll19@student.aau.dk>
2024-03-13 12:38:42 +0100

    Needed extra ports and updated gitignore

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 4000:4000 -v $(pwd):/root/ahll19.github.io -it jekyll:latest bash
+docker run -p 4000:4000 -p 35729:35729 -v $(pwd):/root/ahll19.github.io -it jekyll:latest bash
